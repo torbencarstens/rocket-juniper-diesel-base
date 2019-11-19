4dff1211abd1dae71b774fe0a3f78d0947401395
Torben Carstens <github@carstens.tech>
2019-11-19 19:13:39 +0100

    Add src/database/schema.rs

---
+++ src/database/schema.rs
@@ -0,0 +1,28 @@
+table! {
+    events (id) {
+        id -> Int4,
+        timestamp -> Timestamp,
+        location -> Varchar,
+    }
+}
+
+table! {
+    groups (id) {
+        id -> Int4,
+        name -> Nullable<Varchar>,
+    }
+}
+
+table! {
+    users (id) {
+        id -> Int4,
+        name -> Varchar,
+        age -> Int2,
+    }
+}
+
+allow_tables_to_appear_in_same_query!(
+    events,
+    groups,
+    users,
+);
