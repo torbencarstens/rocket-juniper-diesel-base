b02516a86d902661b56a083ed8cd69bed86dbb75
Torben Carstens <github@carstens.tech>
2019-11-20 00:13:11 +0100

    Add db to rocket

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,12 @@
 #[macro_use]
 extern crate diesel;
 #[macro_use]
+#[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
 
 pub mod database;
 pub mod graphql;
 pub mod routes;
+mod db;
